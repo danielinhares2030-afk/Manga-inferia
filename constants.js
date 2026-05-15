63bc77a549a452d5888c5d89c88b3e142a392ba7
danielinhares2030-afk <danielinhares2030@gmail.com>
2026-05-15 18:54:31 -0300

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,11 @@
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyALxsSUclmKJXCBUFVPyTU9QWBfvjkM0tc",
+  authDomain: "manga-inferia.firebaseapp.com",
+  projectId: "manga-inferia",
+  storageBucket: "manga-inferia.firebasestorage.app",
+  messagingSenderId: "693080808285",
+  appId: "1:693080808285:web:539180b1b290c38d3726b4",
+  measurementId: "G-XMZ4911L2V"
+};
